726572f436971bb3022dfda272d0c38d250f810f
정은 <xeoxxeux@jeong-eun-ui-MacBookPro.local>
2022-06-03 18:42:30 +0900

    0603

---
+++ SCON/Views/Login/LoginScreen.js
@@ -0,0 +1,19 @@
+// /* Splash.js에서 user_id가 확인되지 않으면 LoginScreen.js로 이동하게 된다. */
+
+// const [userId, setUserId] = useState('');
+//   const [userPassword, setUserPassword] = useState('');
+//   const [loading, setLoading] = useState(false);
+//   const [errortext, setErrortext] = useState('');
+
+// <TextInput                                                         
+//           style={styles.textFormTop}
+//           placeholder={'이메일'}
+//           onChangeText={(userId) => setUserId(userId)}
+//           autoCapitalize="none"
+//           returnKeyType="next"
+//           onSubmitEditing={() =>
+//             passwordInputRef.current && passwordInputRef.current.focus()
+//           }
+//           underlineColorAndroid="#f000"
+//           blurOnSubmit={false}
+//         />              
