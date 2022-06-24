f0b0d432daa7ec73c80b29a084ef2a8b0e25cd1c
chichoon <chichoon.choi@gmail.com>
2022-06-25 00:24:00 +0900

    Remove: 필요없는 스타일명 삭제

--- SCON/Views/Home/Head.js
+++ SCON/Views/Home/Head.js
@@ -3,7 +3,7 @@ import {View, Text, Image, TouchableOpacity} from 'react-native';
 
 import {NickName} from './index';
 import LogoImage from '../../Assets/Images/LineupLogo.png'; // svg로 교체 추천
-import AlertImage from '../../Assets/Images/AlertImage.png'; // svg로 교체 추천
+import NotiImage from '../../Assets/Images/NotiImage.png'; // svg로 교체 추천
 
 import styles from './Head.styles';
 
@@ -13,7 +13,7 @@ function AlertIcon({alert, navigation}) {
       <View style={[styles.alignment, styles.alertWrapper]}>
         <TouchableOpacity onPress={() => navigation.navigate('Alert')}>
           <View style={styles.alertIconWrapper}>
-            <Image source={AlertImage} style={styles.alertIcon} />
+            <Image source={NotiImage} />
             {alert ? <View style={styles.alertBadge} /> : <></>}
           </View>
         </TouchableOpacity>
@@ -27,7 +27,7 @@ export function Head({Data, navigation}) {
       <View style={styles.headerWrapper}>
         <View style={styles.alignment} />
         <View style={[styles.alignment, styles.imageWrapper]}>
-          <Image source={LogoImage} style={styles.logoImage} />
+          <Image source={LogoImage} />
         </View>
         <AlertIcon alert={Data.alert} navigation={navigation} />
       </View>
