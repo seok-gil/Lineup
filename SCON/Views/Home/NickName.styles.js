363de3d13ebdb196f351714d867e9912633f96a5
chichoon <chichoon.choi@gmail.com>
2022-06-24 23:11:44 +0900

    Design: Nickname 컴포넌트 스타일링

---
+++ SCON/Views/Home/NickName.styles.js
@@ -0,0 +1,39 @@
+import {StyleSheet} from 'react-native';
+import {colors} from '../../Styles';
+
+const styles = StyleSheet.create({
+  viewWrapper: {
+    paddingHorizontal: 30,
+    paddingVertical: 20,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    flexDirection: 'row',
+    backgroundColor: colors.WHITE,
+  },
+
+  image: {
+    width: 64,
+    height: 64,
+    marginRight: 15,
+    resizeMode: 'contain',
+  },
+
+  viewProfileInfo: {
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+  },
+
+  nameText: {
+    width: 150,
+    fontSize: 24,
+    fontWeight: '700',
+  },
+
+  emailText: {
+    width: 150,
+    color: colors.TEXT_LIGHT,
+  },
+});
+
+export default styles;
