f493b18ea23df745f5be3202ece395bc406a3c0a
chichoon <chichoon.choi@gmail.com>
2022-07-03 01:16:00 +0900

    Design: RecordScreen

--- SCON/Views/Record/RecordScreen.js
+++ SCON/Views/Record/RecordScreen.js
@@ -3,12 +3,14 @@ import {View, Text} from 'react-native';
 
 import RecordHead from './RecordHead';
 import PlayerCard from './PlayerCard';
+
 import Data from '../../Assets/Data/Record.json';
+import styles from './RecordScreen.styles';
 
 export function RecordScreen({navigation}) {
   console.log(Data);
   return (
-    <View style={{flexDirection: 'column'}}>
+    <View style={styles.recordScreenWrapper}>
       <RecordHead medal={Data.Medal} />
       <View>
         {Data.Record.map(record => (
