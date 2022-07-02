0892d10b424ddfadae6de1b50474675f1a1934cd
chichoon <chichoon.choi@gmail.com>
2022-07-02 12:24:57 +0900

    Design: 검색 결과 하단 '결과 모두보기' 스타일링

---
+++ SCON/Views/Search/SearchID/SearchID.styles.js
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'react-native';
+
+import {colors} from '../../../Styles';
+
+const styles = StyleSheet.create({
+  searchExpand: {
+    paddingVertical: 20,
+    alignItems: 'center',
+  },
+
+  expandText: {
+    fontSize: 13,
+    fontWeight: '500',
+  },
+});
+
+export default styles;
