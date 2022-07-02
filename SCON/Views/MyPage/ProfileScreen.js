13774692ec3f837dc1f5852e48242bbf8b0d51c2
hopark <polkm1789@gmail.com>
2022-07-02 15:49:15 +0900

    chore: install react-native-image-picker

--- SCON/Views/MyPage/ProfileScreen.js
+++ SCON/Views/MyPage/ProfileScreen.js
@@ -1,34 +1,41 @@
 import React, { Component } from 'react';
 import {
-	Button,
-	View,
-	Image,
-	Text,
-	StyleSheet,
+  Button,
+  View,
+  Image,
+  Text,
+  StyleSheet,
 } from 'react-native';
 
 import DefaultProfile from '../../Assets/Images/ProfileDefault.png'
 
 export function ProfileScreen({ navigation }) {
-	return (
-		<View style={{ flexDirection: 'column', }}>
-			<Image source={DefaultProfile} style={styles.image} />
-			<Text> 닉네임</Text>
-			<Text> email</Text>
-			<Text> 생년월일</Text>
-			<Text> 성별</Text>
-			<Text> 종목</Text>
-
-		</View>
-	);
+  const data = require('../../Assets/Data/Profile.json').profile
+  console.log(data)
+  return (
+    <View style={{ flexDirection: 'column', }}>
+      <Image source={DefaultProfile} style={styles.image} />
+      <Image source={DefaultProfile} style={styles.image} />
+      <Text>이름</Text>
+      <Text>{data.name}</Text>
+      <Text>이메일 계정</Text>
+      <Text>{data.email}</Text>
+      <Text>생년월일</Text>
+      <Text>{data.birth}</Text>
+      <Text>성별</Text>
+      <Text>{data.sex}</Text>
+      <Text>종목</Text>
+      <Text>{data.major}</Text>
+      <Text>소속</Text>
+      <Text>{data.belong}</Text>
+    </View>
+  );
 }
 
-
-
 const styles = StyleSheet.create({
-	image: {
-		width: '100%',
-		height: '30%',
-		resizeMode: 'contain'
-	},
+  image: {
+    width: '100%',
+    height: '30%',
+    resizeMode: 'contain'
+  },
 });
