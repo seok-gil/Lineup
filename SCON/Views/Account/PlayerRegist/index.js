2d34cb1b557e192ed6770c6bb935be3fbb17c71c
hopark <polkm1789@gmail.com>
2022-07-06 16:30:15 +0900

    chore: install select-dropdown

---
+++ SCON/Views/Account/PlayerRegist/index.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import { useState } from 'react';
+import { View, Image, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import { BirthForm } from "./BirthForm"
+
+export function PlayerRegist({ navigation }) {
+	const [form, setForm] = useState({
+		name: '',
+		birth: '',
+		gender: '',
+		major: '',
+		belong: '',
+	})
+	const [validate, setValidate] = useState({
+		name: '',
+		birth: new Date(),
+		gender: '',
+		major: '',
+		belong: '',
+	})
+	const onInput = (key, e) => {
+		const { text } = e.nativeEvent;
+		setForm({
+			...form,
+			[key]: text,
+		});
+		console.log(form)
+	}
+	return (
+		<View style={{ flexDirection: 'column', }}>
+			<Text>운동선수 확인을 시작합니다.</Text>
+			<Text>확인된 내용이 실제와 다르면 이용이 제한됩니다.</Text>
+			<Text>이름</Text>
+			<TextInput
+				value={form.name}
+				placeholder={'이름을 입력해주세요'}
+				placeholderTextColor="#C5C8CE"
+				onChange={e => onInput('name', e)}
+			/>
+			<BirthForm form={form} setForm={setForm}/>
+			<Text>성별</Text>
+			<TextInput
+				value={form.name}
+				placeholder={'여자/남자 선택해주세요'}
+				placeholderTextColor="#C5C8CE"
+				onChange={e => onInput('name', e)}
+			/>
+			<Text>종목</Text>
+			<TextInput
+				value={form.name}
+				placeholder={'종목을 선택해주세요'}
+				placeholderTextColor="#C5C8CE"
+				onChange={e => onInput('name', e)}
+			/>
+			<Text>소속</Text>
+			<TextInput
+				value={form.name}
+				placeholder={'소속명을 입력해주세요'}
+				placeholderTextColor="#C5C8CE"
+				onChange={e => onInput('name', e)}
+			/>
+		</View>
+	);
+}
+
+
+const styles = StyleSheet.create({
+	image: {
+		width: '100%',
+		height: '30%',
+		resizeMode: 'contain'
+	},
+});
