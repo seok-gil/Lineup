2884054ec9c9141c6977fc85fc9b8cbe4c4cbf25
chichoon <chichoon.choi@gmail.com>
2022-07-14 12:38:02 +0900

    Design: Story 페이지 아이콘 추가

--- SCON/Views/Story/StoryScreen.js
+++ SCON/Views/Story/StoryScreen.js
@@ -1,29 +1,42 @@
-import React, { Component } from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import React, {Component} from 'react';
 
-import {
-	Button,
-	View,
-	Text,
-	StyleSheet,
-	TouchableOpacity,
-	SafeAreaView
-} from 'react-native';
+import {View, Text, TouchableOpacity, SafeAreaView, Image} from 'react-native';
 
+import styles from './StoryScreen.styles';
+import FeedIcon from './assets/feedIcon.png';
+import ResultIcon from './assets/resultIcon.png';
+import ScheduleIcon from './assets/scheduleIcon.png';
 
-export function StoryScreen({ navigation }) {
-	return (
-		<SafeAreaView style={{ flex: 3 }}>
-			<TouchableOpacity onPress={() => navigation.navigate('FeedRegist', { names: ['Brent', 'Satya', 'Michaś'] })}>
-				<Text>피드</Text>
-			</TouchableOpacity>
-			<TouchableOpacity onPress={() => navigation.navigate('CompetitionRegist', { names: ['Brent', 'Satya', 'Michaś'] })}>
-				<Text>대회 일정</Text>
-			</TouchableOpacity>
-			<TouchableOpacity onPress={() => navigation.navigate('CompetitionResult', { names: ['Brent', 'Satya', 'Michaś'] })}>
-				<Text>대회 결과</Text>
-			</TouchableOpacity>
-		</SafeAreaView>
-	);
+export function StoryScreen({navigation}) {
+  return (
+    <SafeAreaView style={{flex: 3}}>
+      <TouchableOpacity
+        onPress={() =>
+          navigation.navigate('FeedRegist', {
+            names: ['Brent', 'Satya', 'Michaś'],
+          })
+        }>
+        <Image source={FeedIcon} style={styles.storyIcon} />
+        <Text>피드</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() =>
+          navigation.navigate('CompetitionRegist', {
+            names: ['Brent', 'Satya', 'Michaś'],
+          })
+        }>
+        <Image source={ScheduleIcon} style={styles.storyIcon} />
+        <Text>대회 일정</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() =>
+          navigation.navigate('CompetitionResult', {
+            names: ['Brent', 'Satya', 'Michaś'],
+          })
+        }>
+        <Image source={ResultIcon} style={styles.storyIcon} />
+        <Text>대회 결과</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
 }
-
