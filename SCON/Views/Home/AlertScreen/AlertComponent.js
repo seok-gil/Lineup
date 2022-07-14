a5c0bb4748f3302b64ff7a77ec9f25c9b7b1cc8d
hopark <polkm1789@gmail.com>
2022-07-14 20:24:41 +0900

    feat:AlertAPI

--- SCON/Views/Home/AlertScreen/AlertComponent.js
+++ SCON/Views/Home/AlertScreen/AlertComponent.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import {View, Text, Image, TouchableOpacity} from 'react-native';
-
 import styles from './AlertComponent.styles';
-
 import {SmileIcon, ChatBubbleIcon, XIcon} from '../../../Assets/Icons';
+import { ApiFetch } from '../../../Components/API/ApiFetch';
 
 function AlertComponent({alert}) {
   if (!alert) return <View />;
 
-  const onClickX = alarmId => {
-    console.log(alarmId);
+  //TODO delete alarmAPI
+  const onClickX = ( alarmId ) => {
+      ApiFetch({
+        method: 'DELETE',
+        url: `http://localhost:1337/api/alerts/2`,
+        headers: { "Authorization": "token" },
+        body: null
+      })
   };
 
   return (
@@ -24,14 +29,13 @@ function AlertComponent({alert}) {
           style={styles.alertComponentImage}
         />
         <Text style={styles.alertTitle}>
-          {alert.check}
-          {alert.type}
+          {alert.type === '댓글'? '댓글 알림' : '선수 대회 알림'}
         </Text>
       </View>
       <View style={styles.alertComponentMiddle}>
         <Text style={styles.alertContent}>{alert.content}</Text>
         <View style={styles.alertComponentBottom}>
-          <Text style={styles.alertCreatedAt}>{alert.createdAt}</Text>
+          <Text style={styles.alertCreatedAt}>{`(`}{alert.date}{`)`}</Text>
           <TouchableOpacity onPress={() => onClickX(alert.alarmId)}>
             <Image source={XIcon} style={styles.alertXIcon} />
           </TouchableOpacity>
