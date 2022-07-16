c295a2a0a558c24f4c1702bc6a0ffe925f0f5986
hopark <polkm1789@gmail.com>
2022-07-16 17:05:25 +0900

    chore:strapi test

---
+++ testAPI/src/api/player-home/controllers/player-home.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  player-home controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::player-home.player-home');
