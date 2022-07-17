9d0f176a55098b730741917d692bc046e471be60
chichoon <chichoon.choi@gmail.com>
2022-07-17 15:50:00 +0900

    Design: ETC

--- SCON/Views/Rank/ETC.js
+++ SCON/Views/Rank/ETC.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {View, Image, Text, TouchableOpacity} from 'react-native';
 
 import styles from './ETC.styles';
+import {HeartIcon} from '../../Assets/Icons';
 import {DefaultProfileImage} from '../../Assets/Images';
 
 function ETC({player, index, navigation}) {
@@ -9,13 +10,22 @@ function ETC({player, index, navigation}) {
     <TouchableOpacity
       onPress={() =>
         navigation.navigate('Player', {names: ['Brent', 'Satya', 'Michaś']})
-      }>
-      <View style={{flexDirection: 'row'}}>
-        <Text>{index}</Text>
-        <Image source={DefaultProfileImage} style={styles.image2} />
-        <Text> {player.player_name}</Text>
-        <Text> {player.player_player_major}</Text>
-        <Text> 좋아요 X {player.player_like} </Text>
+      }
+      style={styles.etcWrapper}>
+      <Text style={styles.index}>{index}</Text>
+      <Image source={DefaultProfileImage} style={styles.profileImage} />
+      <View style={styles.profileWrapper}>
+        <View>
+          <View style={styles.profileInnerWrapper}>
+            <Text style={styles.name}>{player.player_name}</Text>
+            <Text>({player.player_major})</Text>
+          </View>
+          <Text style={styles.from}>소속</Text>
+        </View>
+        <View style={styles.likeCount}>
+          <Image source={HeartIcon} style={styles.heart} />
+          <Text style={styles.likes}>{player.player_like} </Text>
+        </View>
       </View>
     </TouchableOpacity>
   );
--- SCON/Views/Rank/ETC.styles.js
+++ SCON/Views/Rank/ETC.styles.js
@@ -1,6 +1,64 @@
 import {StyleSheet} from 'react-native';
-import {colors} from '../../Styles';
+import {colors, globalTextStyle, listElementStyle} from '../../Styles';
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  etcWrapper: {
+    ...listElementStyle,
+    flexDirection: 'row',
+  },
+
+  index: {
+    color: colors.TEXT_DARK,
+    fontSize: 17,
+    fontWeight: '700',
+    marginRight: 20,
+  },
+
+  profileImage: {
+    width: 45,
+    height: 45,
+    resizeMode: 'contain',
+    marginRight: 20,
+  },
+
+  profileWrapper: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+
+  profileInnerWrapper: {
+    flexDirection: 'row',
+  },
+
+  name: {
+    color: colors.TEXT_DARK,
+    fontSize: 17,
+    fontWeight: '700',
+    marginRight: 3,
+  },
+
+  from: {
+    marginTop: 3,
+    color: colors.TEXT_LIGHT,
+  },
+
+  likeCount: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+
+  heart: {
+    width: 10,
+    resizeMode: 'contain',
+    marginRight: 5,
+  },
+  likes: {
+    color: colors.TEXT_DARK,
+    fontSize: 17,
+    fontWeight: '700',
+  },
+});
 
 export default styles;
