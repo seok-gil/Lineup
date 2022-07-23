6eaafcd8cf19ba9ea7e3aebca3a45cf4345ea0c0
chichoon <chichoon.choi@gmail.com>
2022-07-23 19:24:17 +0900

    Design: NotiElement

---
+++ SCON/Views/Noti/NotiElement.js
@@ -0,0 +1,43 @@
+import React, {Component, useState} from 'react';
+import {
+  Button,
+  View,
+  Image,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
+
+import {DownIcon} from '../../Assets/Icons';
+import styles from './NotiElement.styles';
+
+function NotiElement({data}) {
+  const [expand, setExpand] = useState(false);
+  const onClick = () => {
+    setExpand(!expand);
+  };
+  return (
+    <>
+      <TouchableOpacity onPress={onClick}>
+        <View style={styles.notiElementWrapper}>
+          <View style={styles.notiElementLeft}>
+            <Text style={styles.notiTitle}>{data.title} </Text>
+            <Text style={styles.notiCreated}>{data.createdAt} </Text>
+          </View>
+          {expand ? (
+            <Image style={styles.upIcon} source={DownIcon} />
+          ) : (
+            <Image style={styles.downIcon} source={DownIcon} />
+          )}
+        </View>
+      </TouchableOpacity>
+      {expand && (
+        <View style={styles.expanded}>
+          <Text style={styles.expandedContent}>{data.content}</Text>
+        </View>
+      )}
+    </>
+  );
+}
+
+export default NotiElement;
