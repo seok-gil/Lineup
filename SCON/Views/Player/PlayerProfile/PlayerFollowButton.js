b6c51d7854a2572c6b47544054ffb61bed6598d7
chichoon <chichoon.choi@gmail.com>
2022-07-24 11:49:08 +0900

    Design: PlayerFollowButton

---
+++ SCON/Views/Player/PlayerProfile/PlayerFollowButton.js
@@ -0,0 +1,32 @@
+import React, {useEffect, useState} from 'react';
+import {ApiFetchOne} from '../../../Components/API/ApiFetch';
+import {TouchableOpacity, Text, View} from 'react-native';
+
+import styles from './PlayerFollowButton.styles';
+
+function PlayerFollowButton({data, navigation}) {
+  const [follow, setFollow] = useState(data.isFollow);
+  console.log(data.isMe);
+  console.log(data.isFollow);
+  const buttonStyle =
+    data.isMe & !data.isFollow ? styles.followedButton : styles.followButton;
+
+  const buttonTextStyle =
+    data.isMe & !data.isFollow ? styles.followedText : styles.followText;
+
+  const buttonText = () => {
+    if (!data.isMe) return '스토리 추가하기';
+    else if (data.isFollow) return '나의 라인업 추가됨';
+    else return '나의 라인업 추가하기';
+  };
+
+  return (
+    <TouchableOpacity
+      onPress={() => navigation.navigate('StoryScreen')}
+      style={buttonStyle}>
+      <Text style={buttonTextStyle}>{buttonText()}</Text>
+    </TouchableOpacity>
+  );
+}
+
+export default PlayerFollowButton;
