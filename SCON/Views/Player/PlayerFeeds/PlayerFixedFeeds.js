ad595147781cb3a742b23530b3efe7ae970f3d6e
chichoon <chichoon.choi@gmail.com>
2022-07-24 12:31:55 +0900

    Design: PlayerFixedFeeds

--- SCON/Views/Player/PlayerFeeds/PlayerFixedFeeds.js
+++ SCON/Views/Player/PlayerFeeds/PlayerFixedFeeds.js
@@ -7,6 +7,7 @@ import {
   PinnedIcon,
   TagIcon,
 } from '../../../Assets/Icons';
+import styles from './PlayerFixedFeeds.styles';
 
 export function PlayerFixedFeed({navigation}) {
   const [data, setData] = useState();
@@ -21,40 +22,47 @@ export function PlayerFixedFeed({navigation}) {
       setData(thing);
     });
   }, []);
+
   //TODO comment 보이기
   if (data) {
     const today = new Date().getDate();
     var dDay = data.date.slice(8, 10) - today;
+
     return (
       <TouchableOpacity
         onPress={() =>
           navigation.navigate('FeedScreen', {feedId: `${data.feedId}`})
-        }>
-        <View style={{flexDirection: 'row'}}>
-          <Image source={TagIcon} />
-          <Text>D - {dDay}</Text>
+        }
+        style={styles.fixedFeedWrapper}>
+        <View style={styles.fixedFeedTag}>
+          <View style={styles.fixedFeedTagInner}>
+            <Image source={TagIcon} style={styles.fixedFeedTagImage} />
+            <Text style={styles.fixedFeedTagText}>
+              D{dDay <= 0 ? dDay : '-' + dDay}
+            </Text>
+          </View>
         </View>
-        <View style={{flexDirection: 'row'}}>
-          <Text>고정됨</Text>
-          <Image source={PinnedIcon} />
+        <View style={styles.fixedFeedPin}>
+          <Image source={PinnedIcon} style={styles.fixedFeedPinIcon} />
+          <Text style={styles.fixedFeedPinText}>고정됨</Text>
         </View>
-        <Text>{data.content}</Text>
-        <View style={{flexDirection: 'row'}}>
+        <Text style={styles.fixedFeedContent}>{data.content}</Text>
+        <View style={styles.feedLikedBox}>
           <TouchableOpacity
             onPress={() =>
               navigation.navigate('/', {names: ['Brent', 'Satya', 'Michaś']})
             }
-            style={{flexDirection: 'row'}}>
-            <Image source={HeartEmptyIcon} />
-            <Text>{data.likeCnt}</Text>
+            style={styles.feedLikeBoxInner}>
+            <Image source={HeartEmptyIcon} style={styles.likeIcon} />
+            <Text style={styles.likeText}>{data.likeCnt}</Text>
           </TouchableOpacity>
           <TouchableOpacity
             onPress={() =>
               navigation.navigate('/', {names: ['Brent', 'Satya', 'Michaś']})
             }
-            style={{flexDirection: 'row'}}>
-            <Image source={CommentIcon} />
-            <Text>{data.commentCnt} </Text>
+            style={styles.feedLikeBoxInner}>
+            <Image source={CommentIcon} style={styles.likeIcon} />
+            <Text style={styles.likeText}>{data.commentCnt} </Text>
           </TouchableOpacity>
         </View>
       </TouchableOpacity>
