946950526b0701fd27e43bd2e9eba73e36f11425
chichoon <chichoon.choi@gmail.com>
2022-07-24 11:49:27 +0900

    Design: Player Data

--- SCON/Views/Player/PlayerProfile/PlayerData.js
+++ SCON/Views/Player/PlayerProfile/PlayerData.js
@@ -1,51 +1,56 @@
-import React, { useEffect, useState } from 'react';
-import { Image, View, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, Text, TouchableOpacity} from 'react-native';
 
-export function PlayerData({ data, navigation }) {
-	const [rank, setRank] = useState({
-	  "type": 'ALL',
-	  "rank": '0',
-	})
-	useEffect(() => {
-	  const ranks = data.ranks
-	  const arr = []
-	  for (var i in ranks)
-		arr.push([i, ranks[i]])
-	  arr.sort(function (a, b) {
-		return a[1] - b[1];
-	  });
-	  if (arr[0]) {
-		setRank({
-		  "type": arr[0][0],
-		  "rank": arr[0][1]
-		})
-	  }
-	}, [])
-  
-	return (
-	  <View style={{ flexDirection: 'row' }}>
-		<TouchableOpacity
-		  onPress={() =>
-			navigation.navigate('Record', { names: ['Brent', 'Satya', 'Michaś'] })
-		  }>
-		  <Text>전적</Text>
-		  <Text> {data.recordCnt}</Text>
-		</TouchableOpacity>
-		<TouchableOpacity
-		  onPress={() =>
-			navigation.navigate('Fan', { names: ['Brent', 'Satya', 'Michaś'] })
-		  }>
-		  <Text>팬</Text>
-		  <Text>{data.followCnt}</Text>
-		</TouchableOpacity>
-		<TouchableOpacity
-		  onPress={() =>
-			navigation.navigate('Rank', { names: ['Brent', 'Satya', 'Michaś'] })
-		  }>
-		  <Text>{rank.type.toUpperCase()}</Text>
-		  <Text> {rank.rank}위</Text>
-		</TouchableOpacity>
-	  </View>
-	);
-  }
-  
+import styles from './PlayerData.styles';
+
+function PlayerData({data, navigation}) {
+  const [rank, setRank] = useState({
+    type: 'ALL',
+    rank: '0',
+  });
+  useEffect(() => {
+    const ranks = data.ranks;
+    const arr = [];
+    for (var i in ranks) arr.push([i, ranks[i]]);
+    arr.sort(function (a, b) {
+      return a[1] - b[1];
+    });
+    if (arr[0]) {
+      setRank({
+        type: arr[0][0],
+        rank: arr[0][1],
+      });
+    }
+  }, []);
+
+  return (
+    <View style={styles.playerDataWrapper}>
+      <TouchableOpacity
+        style={styles.playerDataElement}
+        onPress={() =>
+          navigation.navigate('Record', {names: ['Brent', 'Satya', 'Michaś']})
+        }>
+        <Text style={styles.playerDataTitle}>전적</Text>
+        <Text style={styles.playerDataText}>12{/*data.recordCnt*/}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.playerDataElement}
+        onPress={() =>
+          navigation.navigate('Fan', {names: ['Brent', 'Satya', 'Michaś']})
+        }>
+        <Text style={styles.playerDataTitle}>팬</Text>
+        <Text style={styles.playerDataText}>104{/*data.followCnt*/}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.playerDataElement}
+        onPress={() =>
+          navigation.navigate('Rank', {names: ['Brent', 'Satya', 'Michaś']})
+        }>
+        <Text style={styles.playerDataTitle}>{rank.type.toUpperCase()}</Text>
+        <Text style={styles.playerDataText}>7{/*rank.rank*/}위</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+export default PlayerData;
