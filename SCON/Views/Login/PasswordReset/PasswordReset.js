dc24d6750cea3ebbc882dbacfd20001a4d7f71a3
chichoon <chichoon.choi@gmail.com>
2022-07-24 18:00:03 +0900

    Design: PassReset

--- SCON/Views/Login/PasswordReset/PasswordReset.js
+++ SCON/Views/Login/PasswordReset/PasswordReset.js
@@ -1,14 +1,14 @@
 import React, {Component, useState} from 'react';
 import {
   SafeAreaView,
-  View,
-  Image,
-  Text,
-  Alert,
-  TextInput,
   TouchableOpacity,
+  View,
+  Text,
+  TextInput,
 } from 'react-native';
 import {ButtonBig} from '../../../Components';
+
+import styles from './PasswordReset.styles';
 
 export function PasswordReset({navigation}) {
   const [form, setForm] = useState({
@@ -29,26 +29,46 @@ export function PasswordReset({navigation}) {
     });
   };
   return (
-    <SafeAreaView style={{flexDirection: 'column'}}>
-      <Text>새로운 비밀번호를 입력해주세요.</Text>
-      <Text>8~16자의 영문, 숫자, 특수기호를 조합하여 사용.</Text>
-      <TextInput
-        value={form.password}
-        placeholder={'비밀번호'}
-        placeholderTextColor="#0E0E0E66"
-        onChange={e => onInput('email', e)}
-      />
-      {validate.password == false && <Text>올바른 비밀번호가 아닙니다.</Text>}
-      <TextInput
-        value={form.certification}
-        placeholder={'비밀번호 확인'}
-        placeholderTextColor="#0E0E0E66"
-        onChange={e => onInput('certification', e)}
-      />
-      {validate.certification == false && (
-        <Text>상단 비밀번호와 일치하지 않습니다.</Text>
-      )}
-      <ButtonBig text={'확인'} onPress={() => navigation.navigate('Tab')} />
+    <SafeAreaView style={styles.passwordWrapper}>
+      <View style={styles.passwordInner}>
+        <View style={styles.passwordTop}>
+          <Text style={styles.title}>새로운 비밀번호를 입력해주세요.</Text>
+          <Text style={styles.label}>
+            8~16자의 영문, 숫자, 특수기호를 조합하여 사용.
+          </Text>
+          <TextInput
+            value={form.password}
+            style={styles.input}
+            placeholder={'비밀번호'}
+            placeholderTextColor="#0E0E0E66"
+            onChange={e => onInput('email', e)}
+          />
+          {validate.password == false && (
+            <Text style={styles.errorMessage}>올바른 비밀번호가 아닙니다.</Text>
+          )}
+          <TextInput
+            value={form.certification}
+            style={styles.input}
+            placeholder={'비밀번호 확인'}
+            placeholderTextColor="#0E0E0E66"
+            onChange={e => onInput('certification', e)}
+          />
+          {validate.certification == false && (
+            <Text style={styles.errorMessage}>
+              상단 비밀번호와 일치하지 않습니다.
+            </Text>
+          )}
+        </View>
+        <TouchableOpacity
+          onPress={() => navigation.navigate('Tab')}
+          style={
+            validate.password && validate.certification
+              ? styles.loginButton
+              : styles.loginButtonNotAvailable
+          }>
+          <Text style={styles.loginButtonText}>다음</Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
