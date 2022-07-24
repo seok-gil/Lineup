8f3c2fda040a12f0f7cac61db8dd34601e41fe4d
chichoon <chichoon.choi@gmail.com>
2022-07-24 17:59:54 +0900

    Design: ForgetPassword

--- SCON/Views/Login/PasswordReset/ForgetPassword.js
+++ SCON/Views/Login/PasswordReset/ForgetPassword.js
@@ -10,6 +10,8 @@ import {
 } from 'react-native';
 import {ButtonBig} from '../../../Components';
 
+import styles from './ForgetPassword.styles';
+
 export function ForgetPassword({navigation}) {
   const [form, setForm] = useState({
     email: '',
@@ -29,34 +31,57 @@ export function ForgetPassword({navigation}) {
     });
   };
   return (
-    <SafeAreaView style={{flexDirection: 'column'}}>
-      <Text>라인업 가입 정보를 다시 확인할께요.</Text>
-      <TextInput
-        value={form.email}
-        placeholder={'이메일 입력'}
-        placeholderTextColor="#0E0E0E66"
-        onChange={e => onInput('email', e)}
-      />
-      <TouchableOpacity onPress={() => console.log('이메일전송')}>
-        <Text>전송</Text>
-      </TouchableOpacity>
-      {validate.email == false && (
-        <Text>가입 된 정보가 없습니다. 다시 입력해주세요.</Text>
-      )}
-      <Text>메일 확인 후 인증번호를 입력해주세요.</Text>
-      <TextInput
-        value={form.certification}
-        placeholder={'인증번호 입력'}
-        placeholderTextColor="#0E0E0E66"
-        onChange={e => onInput('certification', e)}
-      />
-      {validate.certification == false && (
-        <Text>인증번호가 일치하지 않습니다.</Text>
-      )}
-      <ButtonBig
-        text={'다음'}
-        onPress={() => navigation.navigate('PasswordReset')}
-      />
+    <SafeAreaView style={styles.forgetWrapper}>
+      <View style={styles.forgetInner}>
+        <View style={styles.forgetTop}>
+          <Text style={styles.title}>
+            {'라인업 가입 정보를\n다시 확인할게요.'}
+          </Text>
+          <View style={styles.email}>
+            <TextInput
+              value={form.email}
+              style={styles.input}
+              placeholder={'이메일 입력'}
+              placeholderTextColor="#0E0E0E66"
+              onChange={e => onInput('email', e)}
+            />
+            <TouchableOpacity
+              style={styles.sendButton}
+              onPress={() => console.log('이메일전송')}>
+              <Text style={styles.sendButtonText}>전송</Text>
+            </TouchableOpacity>
+          </View>
+          {validate.email == false && (
+            <Text style={styles.errorMessage}>
+              가입 된 정보가 없습니다. 다시 입력해주세요.
+            </Text>
+          )}
+          <Text style={styles.label}>
+            메일 확인 후 인증번호를 입력해주세요.
+          </Text>
+          <TextInput
+            value={form.certification}
+            style={styles.input}
+            placeholder={'인증번호 입력'}
+            placeholderTextColor="#0E0E0E66"
+            onChange={e => onInput('certification', e)}
+          />
+          {validate.certification == false && (
+            <Text style={styles.errorMessage}>
+              인증번호가 일치하지 않습니다.
+            </Text>
+          )}
+        </View>
+        <TouchableOpacity
+          onPress={() => navigation.navigate('PasswordReset')}
+          style={
+            validate.email && validate.certification
+              ? styles.loginButton
+              : styles.loginButtonNotAvailable
+          }>
+          <Text style={styles.loginButtonText}>다음</Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
