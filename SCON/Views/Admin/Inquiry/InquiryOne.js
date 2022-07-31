309919805429e3cd6f9ca43d1bef469319129a53
chichoon <chichoon.choi@gmail.com>
2022-08-01 01:48:55 +0900

    Design: inquiry

--- SCON/Views/Admin/Inquiry/InquiryOne.js
+++ SCON/Views/Admin/Inquiry/InquiryOne.js
@@ -1,75 +1,77 @@
-import React, { useEffect, useState } from 'react';
-import { SafeAreaView, View, Image, Text, Alert, TextInput, TouchableOpacity } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, Image, Text, TextInput, TouchableOpacity} from 'react-native';
 
+import {DownIcon} from '../../../Assets/Icons';
+import {Time} from '../../../Components/Time';
 
-import { DownIcon } from '../../../Assets/Icons'
-import { Time } from "../../../Components/Time"
+import styles from './InquiryOne.styles';
 
-export function InquiryOne({ data }) {
-  if (!data) return <View />
+export function InquiryOne({data}) {
+  if (!data) return <View />;
 
   const [expand, setExpand] = useState(false);
-  const [answer, setAnswer] = useState(data.AnswerContent)
-  const [state, setState] = useState(data.AnswerState)
+  const [answer, setAnswer] = useState(data.AnswerContent);
+  const [state, setState] = useState(false);
 
   const onExpand = () => {
     setExpand(!expand);
   };
 
-  const onInput = (e) => {
-    const { text } = e.nativeEvent;
-    setAnswer(text)
-  }
+  const onInput = e => {
+    const {text} = e.nativeEvent;
+    setAnswer(text);
+  };
 
   const onRegist = () => {
-    console.log("a". state)
+    console.log('a'.state);
     setState(!state);
   };
-  
+
   return (
     <View>
-      {data.AnswerState ?
-        <Text>
-          답변완료 {`>`} {data.InquiryTitle}{' '}
-        </Text>
-        :
-        <Text>
-          접수 {`>`} {data.InquiryTitle}{' '}
-        </Text>
-      }
-      <Text><Time time={data.InquiryDateTime} separator="-"/> </Text>
-      <TouchableOpacity onPress={onExpand}>
-        {expand ? <Image source={DownIcon} /> : <Image source={DownIcon} />}
+      <TouchableOpacity
+        onPress={onExpand}
+        style={styles.inquiryListElementWrapper}>
+        <View style={styles.inquiryLeft}>
+          <Text style={styles.qnaInfo}>
+            {data.AnswerState ? '답변완료' : '접수'} {`>`} {data.InquiryTitle}
+          </Text>
+          <Text style={styles.qnaCreated}>
+            <Time time={data.InquiryDateTime} separator="-" />
+          </Text>
+        </View>
+        <Image
+          source={DownIcon}
+          style={expand ? styles.upIcon : styles.downIcon}
+        />
       </TouchableOpacity>
-      <View>
-        {expand ?
-          <View>
-            <Text>Q.</Text>
-            <Text>{data.InquiryContent} </Text>
-            <Text>A.</Text>
-            {state ?
-              <View>
-                <Text>
-                  {answer}
-                </Text>
-              </View>
-              :
-              <View>
-                <TextInput
-                  value={answer}
-                  placeholder={'내용을 입력해주세요.'}
-                  placeholderTextColor="#0E0E0E66"
-                  onChange={(e) => onInput(e)} />
-              </View>
-            }
-            <TouchableOpacity onPress={onRegist}>
-              <Text>{state ? '수정' : '등록'} </Text>
-            </TouchableOpacity>
+      {expand && (
+        <View style={styles.inquiryQnAWrapper}>
+          <View style={styles.inquiryQuestion}>
+            <Text style={styles.letter}>Q.</Text>
+            <Text style={styles.content}>{data.InquiryContent} </Text>
           </View>
-          :
-          <View />
-        }
-      </View>
+          <View style={styles.inquiryQuestion}>
+            <Text style={styles.letter}>A.</Text>
+            {state ? (
+              <Text style={styles.content}>{answer}</Text>
+            ) : (
+              <TextInput
+                style={styles.inquiryInput}
+                value={answer}
+                placeholder={'내용을 입력해주세요.'}
+                placeholderTextColor="#0E0E0E66"
+                onChange={e => onInput(e)}
+              />
+            )}
+            <View style={styles.buttonWrapper}>
+              <TouchableOpacity onPress={onRegist} style={styles.button}>
+                <Text style={styles.buttonText}>{state ? '수정' : '등록'}</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      )}
     </View>
   );
 }
---
+++ SCON/Views/Admin/Inquiry/InquiryOne.styles.js
@@ -0,0 +1,82 @@
+import {StyleSheet} from 'react-native';
+import {
+  colors,
+  globalButtonStyle,
+  globalButtonTextStyle,
+  globalLargeInputStyle,
+  listElementStyle,
+} from '../../../Styles';
+
+const styles = StyleSheet.create({
+  inquiryListElementWrapper: {
+    ...listElementStyle,
+    backgroundColor: colors.WHITE,
+  },
+
+  inquiryLeft: {
+    flex: 1,
+  },
+
+  qnaInfo: {
+    marginBottom: 5,
+    color: colors.TEXT_DARK,
+  },
+
+  qnaCreated: {
+    fontSize: 12,
+    color: colors.TEXT_LIGHT,
+  },
+
+  downIcon: {
+    width: 11,
+    height: 6,
+  },
+
+  upIcon: {
+    width: 11,
+    height: 6,
+    transform: [{rotateZ: '180deg'}],
+  },
+
+  inquiryQnAWrapper: {
+    backgroundColor: colors.GRAYF6,
+  },
+
+  inquiryQuestion: {
+    padding: 30,
+  },
+
+  letter: {
+    color: colors.TEXT_DARK,
+    fontWeight: '700',
+    fontSize: 17,
+    marginBottom: 8,
+  },
+
+  content: {
+    color: colors.TEXT_DARK,
+    fontWeight: '500',
+  },
+
+  inquiryInput: {
+    ...globalLargeInputStyle,
+    backgroundColor: colors.WHITE,
+    marginBottom: 10,
+  },
+
+  buttonWrapper: {
+    width: '100%',
+    alignItems: 'flex-end',
+  },
+
+  button: {
+    ...globalButtonStyle,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 100,
+  },
+
+  buttonText: globalButtonTextStyle,
+});
+
+export default styles;
