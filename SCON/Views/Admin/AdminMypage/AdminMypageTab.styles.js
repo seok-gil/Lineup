17620c073b4c7c21c916b0740dd4f6fd35e90d20
chichoon <chichoon.choi@gmail.com>
2022-08-01 00:49:55 +0900

    Design: MypageTab

---
+++ SCON/Views/Admin/AdminMypage/AdminMypageTab.styles.js
@@ -0,0 +1,39 @@
+import {StyleSheet} from 'react-native';
+import {colors, listElementStyle} from '../../../Styles';
+
+const styles = StyleSheet.create({
+  mypageTabWrapper: {
+    flexDirection: 'column',
+    flex: 1,
+    backgroundColor: colors.WHITE,
+  },
+
+  mypageElement: {
+    ...listElementStyle,
+  },
+
+  elementLeft: {
+    flexDirection: 'row',
+    flex: 1,
+  },
+
+  elementText: {
+    marginLeft: 20,
+    fontWeight: '700',
+  },
+
+  elementIcon: {
+    width: 18,
+    height: 18,
+    resizeMode: 'contain',
+  },
+
+  arrowIcon: {
+    width: 11,
+    height: 6,
+    resizeMode: 'contain',
+    transform: [{rotateZ: '270deg'}],
+  },
+});
+
+export default styles;
