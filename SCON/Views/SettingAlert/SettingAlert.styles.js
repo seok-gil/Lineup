eac3ff6f4822d15c38a9940e44696f9366d9c2ed
chichoon <chichoon.choi@gmail.com>
2022-08-07 23:26:11 +0900

    Design: SettingsAlert

---
+++ SCON/Views/SettingAlert/SettingAlert.styles.js
@@ -0,0 +1,29 @@
+import {StyleSheet} from 'react-native';
+
+import {
+  colors,
+  globalButtonStyle,
+  globalTextStyle,
+  listElementStyle,
+} from '../../Styles';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: '100%',
+    backgroundColor: colors.WHITE,
+  },
+
+  switchWrapper: {
+    ...listElementStyle,
+    justifyContent: 'space-between',
+  },
+
+  labelText: {
+    ...globalTextStyle,
+    fontSize: 14,
+    fontWeight: '700',
+  },
+});
+
+export default styles;
--- SCON/Views/SettingAlert/index.js
+++ SCON/Views/SettingAlert/index.js
@@ -1,83 +1,74 @@
-import React, { useEffect, useState } from 'react';
-import {
-	View,
-	Text,
-	StyleSheet,
-	Switch
-} from 'react-native';
-import AsyncStorage from "@react-native-community/async-storage"
-import { ApiFetch } from "../../Components/"
-export function SettingAlertScreen({ navigation }) {
+import React, {useEffect, useState} from 'react';
+import {View, Text, StyleSheet, Switch} from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import {ApiFetch} from '../../Components/';
 
-	const [alert, setAlert] = useState({
-		"feedAlarm": true,
-		"commentAlarm": false
-	})
+import styles from './SettingAlert.styles';
 
-	const onClick = (key) => {
-		const set = !alert[key]
-		setAlert({
-			...alert,
-			[key]: set
-		})
-		AsyncStorage.getItem("accessToken")
-			.then((thing) => {
-				ApiFetch({
-					method: 'PUT',
-					url: `/alarm-setting/${key.slice(0,-5)}`,
-					headers: {
-						'content-type': 'application/json',
-						'Authorization': 'Bearer ' + thing,
-					},
-					body: null,
-				})
-			})
-	}
+export function SettingAlertScreen({navigation}) {
+  const [alert, setAlert] = useState({
+    feedAlarm: true,
+    commentAlarm: false,
+  });
 
-	useEffect(() => {
-		AsyncStorage.getItem("accessToken")
-			.then((thing) => {
-				ApiFetch({
-					method: 'GET',
-					url: `/alarm-setting`,
-					headers: {
-						'content-type': 'application/json',
-						'Authorization': 'Bearer ' + thing,
-					},
-					body: null,
-				}).then(thing => {
-					console.log("GETAlert", thing)
-					setAlert(thing)
-				})
-			})
-	}, []);
+  const onClick = key => {
+    const set = !alert[key];
+    setAlert({
+      ...alert,
+      [key]: set,
+    });
+    AsyncStorage.getItem('accessToken').then(thing => {
+      ApiFetch({
+        method: 'PUT',
+        url: `/alarm-setting/${key.slice(0, -5)}`,
+        headers: {
+          'content-type': 'application/json',
+          Authorization: 'Bearer ' + thing,
+        },
+        body: null,
+      });
+    });
+  };
 
-	return (
-		<View style={styles.container}>
-			<Text>선수 피드 알림</Text>
-			<Switch
-				trackColor={{ false: '#767577', true: '#81b0ff' }}
-				thumbColor={alert.feedAlarm ? '#f5dd4b' : '#f4f3f4'}
-				ios_backgroundColor='#3e3e3e'
-				onValueChange={() => onClick('feedAlarm')}
-				value={alert.feedAlarm}
-			/>
-			<Text>댓글 알림</Text>
-			<Switch
-				trackColor={{ false: '#767577', true: '#81b0ff' }}
-				thumbColor={alert.commentAlarm ? '#f5dd4b' : '#f4f3f4'}
-				ios_backgroundColor='#3e3e3e'
-				onValueChange={() => onClick('commentAlarm')}
-				value={alert.commentAlarm}
-			/>
+  useEffect(() => {
+    AsyncStorage.getItem('accessToken').then(thing => {
+      ApiFetch({
+        method: 'GET',
+        url: '/alarm-setting',
+        headers: {
+          'content-type': 'application/json',
+          Authorization: 'Bearer ' + thing,
+        },
+        body: null,
+      }).then(thing => {
+        console.log('GETAlert', thing);
+        setAlert(thing);
+      });
+    });
+  }, []);
 
-		</View>
-	);
+  return (
+    <View style={styles.container}>
+      <View style={styles.switchWrapper}>
+        <Text style={styles.labelText}>선수 피드 알림</Text>
+        <Switch
+          trackColor={{false: '#E6E6E6', true: '#17D3F0'}}
+          thumbColor="#ffffff"
+          ios_backgroundColor="#E6E6E6"
+          onValueChange={() => onClick('feedAlarm')}
+          value={alert.feedAlarm}
+        />
+      </View>
+      <View style={styles.switchWrapper}>
+        <Text style={styles.labelText}>댓글 알림</Text>
+        <Switch
+          trackColor={{false: '#E6E6E6', true: '#17D3F0'}}
+          thumbColor="#ffffff"
+          ios_backgroundColor="#E6E6E6"
+          onValueChange={() => onClick('commentAlarm')}
+          value={alert.commentAlarm}
+        />
+      </View>
+    </View>
+  );
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: 'center',
-	},
-})
