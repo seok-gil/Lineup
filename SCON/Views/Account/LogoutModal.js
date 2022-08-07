af3100bc5a49e374b8512d4e3aa9311e64d772c4
chichoon <chichoon.choi@gmail.com>
2022-08-07 23:19:25 +0900

    Design: Login Modal

--- SCON/Views/Account/LogoutModal.js
+++ SCON/Views/Account/LogoutModal.js
@@ -1,57 +1,50 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   SafeAreaView,
   TouchableOpacity,
   View,
   Text,
   Image,
-  Modal
+  Modal,
 } from 'react-native';
-import AsyncStorage from "@react-native-community/async-storage"
-import { ApiFetch } from '../../Components';
+import AsyncStorage from '@react-native-community/async-storage';
+import {ApiFetch} from '../../Components';
 import styles from './Logout.styles';
 
-export function LogoutModal({ modal, setModal, navigation }) {
+export function LogoutModal({modal, setModal, navigation}) {
   const onPressOn = () => {
-    AsyncStorage.getItem("accessToken")
-      .then((key) => {
-        AsyncStorage.removeItem(key)
-        navigation.navigate('LoginPage')
-        setModal(false)
-      })
-  }
-  
+    AsyncStorage.getItem('accessToken').then(key => {
+      AsyncStorage.removeItem(key);
+      navigation.navigate('LoginPage');
+      setModal(false);
+    });
+  };
+
   const onPressOff = () => {
-    setModal(false)
-  }
+    setModal(false);
+  };
 
   return (
-    <Modal
-      visible={modal}
-    >
+    <Modal visible={modal}>
       <View style={styles.centeredView}>
         <View style={styles.modalView}>
           <View style={styles.modalTop}>
-            <View>
-              <Text>
-                로그이웃하시겠어요
-              </Text>
-            </View>
-            <View>
-              <TouchableOpacity onPress={() => onPressOn()}>
-                <Text>
-                  확인
-                </Text>
-              </TouchableOpacity>
-              <TouchableOpacity onPress={() => onPressOff()}>
-                <Text>
-                  취소
-                </Text>
-              </TouchableOpacity>
-            </View>
+            <Text style={styles.modalText}>로그아웃 하시겠어요?</Text>
+          </View>
+          <View style={styles.modalBottom}>
+            <TouchableOpacity
+              onPress={() => onPressOn()}
+              style={styles.cancelButton}>
+              <Text style={styles.buttonText}>취소</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => onPressOff()}
+              style={styles.submitButton}>
+              <Text style={styles.buttonText}>확인</Text>
+            </TouchableOpacity>
           </View>
         </View>
       </View>
     </Modal>
-  )
+  );
 }
