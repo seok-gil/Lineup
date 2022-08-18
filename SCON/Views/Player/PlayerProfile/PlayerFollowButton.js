7631adb3c51565ff0dc1ceec4014a2ec4f48706a
chichoon <chichoon.choi@gmail.com>
2022-08-18 14:05:52 +0900

    Fix: 조건문

--- SCON/Views/Player/PlayerProfile/PlayerFollowButton.js
+++ SCON/Views/Player/PlayerProfile/PlayerFollowButton.js
@@ -1,64 +1,57 @@
-import React, { useEffect, useState } from 'react'
-import { TouchableOpacity, Text } from 'react-native'
-import { ApiFetch } from '../../../Components'
+import React, {useEffect, useState} from 'react'
+import {TouchableOpacity, Text} from 'react-native'
+import {ApiFetch} from '../../../Components'
 import styles from './PlayerFollowButton.styles'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
-function PlayerFollowButton({ data, navigation, setMount={setMount}}) {
-    const [follow, setFollow] = useState(data.isFollow)
+function PlayerFollowButton({data, navigation, setMount = {setMount}}) {
+  const [follow, setFollow] = useState(data.isFollow)
 
-    const buttonStyle =
-        data.isMe & !data.isFollow ? styles.followedButton : styles.followButton
-    const buttonTextStyle =
-        data.isMe & !data.isFollow ? styles.followedText : styles.followText
+  const buttonStyle =
+    data.isMe || data.isFollow ? styles.followedButton : styles.followButton
+  const buttonTextStyle =
+    data.isMe || data.isFollow ? styles.followedText : styles.followText
 
-    useEffect(() => {
-        AsyncStorage.getItem('role')
-            .then((res) => {
-            })
-    }, [])
+  useEffect(() => {
+    AsyncStorage.getItem('role').then(res => {})
+  }, [])
 
-    const buttonText = () => {
-        if (data.isMe) return '스토리 추가하기'
-        else if (data.isFollow) return '나의 라인업 추가됨'
-        else return '나의 라인업 추가하기'
+  const buttonText = () => {
+    if (data.isMe) return '스토리 추가하기'
+    else if (data.isFollow) return '나의 라인업 추가됨'
+    else return '나의 라인업 추가하기'
+  }
+
+  const followApi = (method, url) => {
+    AsyncStorage.getItem('accessToken').then(thing => {
+      ApiFetch({
+        method: method,
+        url: url,
+        headers: {
+          'content-type': 'application/json',
+          Authorization: 'Bearer ' + thing,
+        },
+        body: null,
+      }).then(() => {
+        setMount(true)
+      })
+    })
+  }
+  const onClick = () => {
+    if (data.isMe) {
+      navigation.navigate('StoryScreen')
+    } else if (data.isFollow) {
+      followApi('DELETE', `/follow/${data.playerId}/${data.isFollow}`)
+    } else {
+      followApi('POST', `/follow/${data.playerId}`)
     }
+  }
 
-    const followApi = (method, url) => {
-        AsyncStorage.getItem('accessToken')
-            .then((thing) => {
-                ApiFetch({
-                    method: method,
-                    url: url,
-                    headers: {
-                        'content-type': 'application/json',
-                        'Authorization': 'Bearer ' + thing,
-                    },
-                    body: null,
-                }).then(() => {
-                    setMount(true)
-                })
-            })
-    }
-    const onClick = () => {
-        if (data.isMe) {
-            navigation.navigate('StoryScreen')
-        }
-        else if (data.isFollow) {
-            followApi('DELETE', `/follow/${data.playerId}/${data.isFollow}`)
-        }
-        else {
-            followApi('POST', `/follow/${data.playerId}`)
-        }
-    }
-
-    return (
-        <TouchableOpacity
-            onPress={() => onClick()}
-            style={buttonStyle}>
-            <Text style={buttonTextStyle}>{buttonText()}</Text>
-        </TouchableOpacity>
-    )
+  return (
+    <TouchableOpacity onPress={() => onClick()} style={buttonStyle}>
+      <Text style={buttonTextStyle}>{buttonText()}</Text>
+    </TouchableOpacity>
+  )
 }
 
 export default PlayerFollowButton
