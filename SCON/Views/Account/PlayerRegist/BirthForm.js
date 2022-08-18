ca25227c8ef08c80400717bed5ef3b7f5f679e36
chichoon <chichoon.choi@gmail.com>
2022-08-18 12:39:53 +0900

    Design: BirthForm

--- SCON/Views/Account/PlayerRegist/BirthForm.js
+++ SCON/Views/Account/PlayerRegist/BirthForm.js
@@ -1,56 +1,50 @@
 import React, {Component, useState} from 'react'
-import {
-    View,
-    Image,
-    Text,
-    StyleSheet,
-    TextInput,
-    TouchableOpacity,
-} from 'react-native'
+import {View, Image, Text, TextInput, TouchableOpacity} from 'react-native'
 import DateTimePickerModal from 'react-native-modal-datetime-picker'
-import {CheckSmallIcon} from '../../../Assets/Icons'
+import {DownIcon} from '../../../Assets/Icons'
+import styles from './BirthForm.styles'
 
-export function BirthForm({form, setForm, setMount}) {
-    const [pickOn, setPickOn] = useState(false)
-    const [text, setText] = useState()
-    const showDatePicker = () => {
-        setPickOn(true)
-    }
-    const handleConfirm = date => {
-        let dateString =
+export function BirthForm({form, setForm, validate}) {
+  const [pickOn, setPickOn] = useState(false)
+  const [text, setText] = useState()
+  const showDatePicker = () => {
+    setPickOn(true)
+  }
+  const handleConfirm = date => {
+    let dateString =
       date.getFullYear() +
-      '년' +
+      '년 ' +
       (date.getMonth() + 1) +
-      '월' +
+      '월 ' +
       date.getDate() +
       '일'
-        setText(dateString)
-        setForm({...form, ['birth']: date})
-        hideDatePicker()
-        setMount('birth')
-    }
+    console.log(date)
+    setText(dateString)
+    setForm({...form, ['birth']: date})
+    hideDatePicker()
+  }
 
-    const hideDatePicker = () => {
-        setPickOn(false)
-    }
-    return (
-        <View style={{flexDirection: 'column'}}>
-            <TouchableOpacity onPress={showDatePicker}>
-                <Text>생년월일</Text>
-                <TextInput
-                    value={text}
-                    placeholder={'생년월일을 선택해주세요'}
-                    placeholderTextColor="#0E0E0E66"
-                    editable={false}
-                />
-                <DateTimePickerModal
-                    isVisible={pickOn}
-                    mode="date"
-                    confirmTextIOS="선택"
-                    onConfirm={handleConfirm}
-                    onCancel={hideDatePicker}
-                />
-            </TouchableOpacity>
+  const hideDatePicker = () => {
+    setPickOn(false)
+  }
+  return (
+    <View style={styles.birthWrapper}>
+      <Text style={styles.label}>생년월일</Text>
+      <TouchableOpacity onPress={showDatePicker}>
+        <View style={styles.inputWrapper}>
+          <Text style={styles.input}>
+            {text ? text : '생년월일을 선택해주세요'}
+          </Text>
+          <Image source={DownIcon} style={styles.downIcon} />
         </View>
-    )
+      </TouchableOpacity>
+      <DateTimePickerModal
+        isVisible={pickOn}
+        mode="date"
+        confirmTextIOS="선택"
+        onConfirm={handleConfirm}
+        onCancel={hideDatePicker}
+      />
+    </View>
+  )
 }
---
+++ SCON/Views/Account/PlayerRegist/BirthForm.styles.js
@@ -0,0 +1,38 @@
+import {StyleSheet} from 'react-native'
+
+import {colors} from '../../../Styles'
+
+const styles = StyleSheet.create({
+  birthWrapper: {
+    flexDirection: 'column',
+    width: '100%',
+  },
+
+  label: {
+    color: colors.TEXT_DARK,
+    fontSize: 14,
+  },
+
+  inputWrapper: {
+    position: 'relative',
+    marginBottom: 10,
+    borderBottomWidth: 2,
+    borderBottomColor: colors.GRAYE6,
+  },
+
+  input: {
+    width: '100%',
+    paddingVertical: 10,
+    color: colors.TEXT_LIGHT,
+  },
+
+  downIcon: {
+    position: 'absolute',
+    top: 15,
+    right: 10,
+    resizeMode: 'contain',
+    width: 15,
+  },
+})
+
+export default styles
