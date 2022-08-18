bcf1e1dacf0a35c3c71ae3ed471e54fc3e3303c4
chichoon <chichoon.choi@gmail.com>
2022-08-18 11:56:22 +0900

    Design: home do

--- SCON/Views/Home/Body/CardList.js
+++ SCON/Views/Home/Body/CardList.js
@@ -5,63 +5,69 @@ import PlayerCard from './PlayerCard'
 
 import styles from './CardList.styles'
 
-export function CardList({ data, role, navigation }) {
-    const follow = data.follows
-    const card = []
-    const emptyData = { playerId: null }
-    const [page, setPage] = useState(0)
-    if (role == 'ROLE_PLAYER') card.push(data.player)
+export function CardList({data, role, navigation}) {
+  const follow = data.follows
+  const card = []
+  const emptyData = {playerId: null}
+  const [page, setPage] = useState(0)
+  if (role == 'ROLE_PLAYER') card.push(data.player)
 
-    for (let i = 0; i < 3; i++) {
-        if (follow && follow[i]) {
-            card.push(follow[i])
-        } else {
-            card.push(emptyData)
-        }
+  for (let i = 0; i < 3; i++) {
+    if (follow && follow[i]) {
+      card.push(follow[i])
+    } else {
+      card.push(emptyData)
     }
+  }
 
-    const onScroll = e => {
-        const newPage = Math.round(
-            e.nativeEvent.contentOffset.x / (pageWidth + gap),
-        )
-        setPage(newPage)
-    }
-
-    const screenWidth = Math.round(Dimensions.get('window').width)
-    const pageWidth = 223
-    const offset = 46
-    const gap = (screenWidth - 223 - 46 * 2) / 2
-
-    function Indicator({focused}) {
-        if (focused === page) {
-            return <View style={styles.activeDot} />
-        } else {
-            return <View style={styles.dot} />
-        }
-    }
-    return (
-        <View>
-            <FlatList
-                data={card}
-                horizontal
-                snapToAlignment="start"
-                decelerationRate="fast"
-                renderItem={({ item, index }) => (
-                    <PlayerCard data={data} item={item} navigation={navigation} index={index} role={role} />
-                )}
-                onScroll={onScroll}
-                pagingEnabled
-                showsHorizontalScrollIndicator={false}
-                snapToInterval={pageWidth + gap}
-                contentContainerStyle={{
-                    paddingHorizontal: offset + gap / 2,
-                }}
-            />
-            <View style={styles.indicatorWrapper}>
-                {Array.from({length: card.length}, (_, i) => (
-                    <Indicator key={`indicator_${i}`} focused={i} />
-                ))}
-            </View>
-        </View>
+  const onScroll = e => {
+    const newPage = Math.round(
+      e.nativeEvent.contentOffset.x / (pageWidth + gap),
     )
+    setPage(newPage)
+  }
+
+  const screenWidth = Math.round(Dimensions.get('window').width)
+  const pageWidth = 223
+  const offset = 46
+  const gap = (screenWidth - 223 - 46 * 2) / 2
+
+  function Indicator({focused}) {
+    if (focused === page) {
+      return <View style={styles.activeDot} />
+    } else {
+      return <View style={styles.dot} />
+    }
+  }
+  return (
+    <View style={styles.cardListWrapper}>
+      <FlatList
+        data={card}
+        horizontal
+        snapToAlignment="start"
+        decelerationRate="fast"
+        renderItem={({item, index}) => (
+          <PlayerCard
+            data={data}
+            item={item}
+            navigation={navigation}
+            index={index}
+            role={role}
+          />
+        )}
+        onScroll={onScroll}
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        snapToInterval={pageWidth + gap}
+        contentContainerStyle={{
+          paddingHorizontal: offset + gap / 2,
+        }}
+      />
+      <View style={styles.indicatorWrapper}>
+        {Array.from({length: card.length}, (_, i) => (
+          <Indicator key={`indicator_${i}`} focused={i} />
+        ))}
+      </View>
+    </View>
+  )
 }
--- SCON/Views/Login/PasswordReset/ForgetPassword.styles.js
+++ SCON/Views/Login/PasswordReset/ForgetPassword.styles.js
@@ -3,81 +3,81 @@ import {StyleSheet} from 'react-native'
 import {colors, globalButtonStyle, globalButtonTextStyle} from '../../../Styles'
 
 const styles = StyleSheet.create({
-    forgetWrapper: {
-        flexDirection: 'column',
-        backgroundColor: colors.WHITE,
-        height: '100%',
-    },
+  forgetWrapper: {
+    flexDirection: 'column',
+    backgroundColor: colors.WHITE,
+    height: '100%',
+  },
 
-    forgetInner: {
-        padding: 30,
-        height: '100%',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-    },
+  forgetInner: {
+    padding: 30,
+    height: '100%',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
 
-    forgetTop: {
-        width: '100%',
-    },
+  forgetTop: {
+    width: '100%',
+  },
 
-    title: {
-        fontWeight: '700',
-        color: colors.TEXT_DARK,
-        fontSize: 23,
-        marginBottom: 30,
-    },
+  title: {
+    fontWeight: '700',
+    color: colors.TEXT_DARK,
+    fontSize: 23,
+    marginBottom: 30,
+  },
 
-    input: {
-        width: '100%',
-        paddingVertical: 10,
-        marginBottom: 10,
-        borderBottomColor: colors.GRAYE6,
-        borderBottomWidth: 2,
-    },
+  input: {
+    width: '100%',
+    paddingVertical: 10,
+    marginBottom: 10,
+    borderBottomColor: colors.GRAYE6,
+    borderBottomWidth: 2,
+  },
 
-    label: {
-        fontWeight: '700',
-        color: colors.TEXT_DARK,
-        fontSize: 14,
-    },
+  label: {
+    fontWeight: '700',
+    color: colors.TEXT_DARK,
+    fontSize: 14,
+  },
 
-    errorMessageWrapper: {
-        height: 30,
-    },
+  errorMessageWrapper: {
+    height: 30,
+  },
 
-    errorMessage: {
-        position: 'absolute',
-        top: 2,
-        color: colors.RED,
-        fontSize: 12,
-    },
+  errorMessage: {
+    position: 'absolute',
+    top: 2,
+    color: colors.RED,
+    fontSize: 12,
+  },
 
-    email: {
-        position: 'relative',
-    },
+  email: {
+    position: 'relative',
+  },
 
-    sendButton: {
-        position: 'absolute',
-        top: 11,
-        right: 5,
-        height: 16,
-    },
+  sendButton: {
+    position: 'absolute',
+    top: 11,
+    right: 5,
+    height: 16,
+  },
 
-    sendButtonText: {
-        color: colors.THEME_SKYBLUE,
-    },
-    sendButtonOffText: {
-        color: colors.TEXT_LIGHT,
-    },
+  sendButtonText: {
+    color: colors.THEME_SKYBLUE,
+  },
+  sendButtonOffText: {
+    color: colors.TEXT_LIGHT,
+  },
 
-    loginButton: globalButtonStyle,
+  loginButton: globalButtonStyle,
 
-    loginButtonNotAvailable: {
-        ...globalButtonStyle,
-        backgroundColor: colors.THEME_SKYBLUE_LIGHT_SOLID,
-    },
+  loginButtonNotAvailable: {
+    ...globalButtonStyle,
+    backgroundColor: colors.THEME_SKYBLUE_LIGHT_SOLID,
+  },
 
-    loginButtonText: globalButtonTextStyle,
+  loginButtonText: globalButtonTextStyle,
 })
 
 export default styles
