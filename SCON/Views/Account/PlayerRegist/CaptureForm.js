9c81831bd0bd859d67b001941fbbcb42867f60b7
chichoon <chichoon.choi@gmail.com>
2022-08-18 13:50:31 +0900

    Design: Player Regist result

--- SCON/Views/Account/PlayerRegist/CaptureForm.js
+++ SCON/Views/Account/PlayerRegist/CaptureForm.js
@@ -1,12 +1,5 @@
 import React, {Component, useState} from 'react'
-import {
-  View,
-  Image,
-  Text,
-  Alert,
-  TextInput,
-  TouchableOpacity,
-} from 'react-native'
+import {View, Image} from 'react-native'
 import {ImagePush} from '../../../Components'
 import {PlayerPhotoPick} from './PlayerPhotoPick'
 import {DefaultProfileImage, PlayerCardImage} from '../../../Assets/Images'
@@ -15,7 +8,7 @@ import styles from './CaptureForm.styles'
 export function CaptureForm({playerPhoto, setPlayerPhoto, setMount}) {
   return (
     <View style={styles.captureWrapper}>
-      <Image source={PlayerCardImage} />
+      <Image source={{uri: playerPhoto.uri}} />
       <PlayerPhotoPick
         text="선수 이미지"
         photo={playerPhoto}
--- SCON/Views/Account/PlayerRegist/PlayerRegistReusltPage.js
+++ SCON/Views/Account/PlayerRegist/PlayerRegistReusltPage.js
@@ -1,70 +1,69 @@
 import React, {useEffect, useState} from 'react'
 import {View, Text, Image, TouchableOpacity} from 'react-native'
 
-import {DefaultProfileImage} from '../../../Assets/Images/index'
+import {
+  PlayerRegistCheckingImage,
+  PlayerRegistDeniedImage,
+  PlayerRegistDoneImage,
+} from '../../../Assets/Images/index'
+import styles from './PlayerRegistResultPage.styles'
 
 export function PlayerRegistResultPage({data, navigation, code}) {
-    const [type, setType] = useState(code)
+  const [type, setType] = useState(code)
 
-    useEffect(() => {
+  const image =
+    {
+      0: PlayerRegistCheckingImage,
+      1: PlayerRegistDoneImage,
+      2: PlayerRegistDeniedImage,
+    }[code] && null
 
-    },[type])
-    type >= 0
-        ? {
-            0: DefaultProfileImage,
-            1: DefaultProfileImage,
-            2: DefaultProfileImage,
-        }[type]
-        : null
+  const head =
+    code >= 0
+      ? {
+          0: '선수 등록 정보를\n심사하고 있습니다.',
+          1: '선수 인증이\n승인되었습니다.',
+          2: '선수 인증 신청이\n반려되었습니다.',
+        }[code]
+      : ''
 
-    const head =
-    type >= 0 ? (
-        {
-            0: '선수 등록 정보를\n심사하고 있습니다.',
-            1: '선수 인증이\n승인되었습니다.',
-            2: '선수 인증 신청이\n반려되었습니다.',
-        }[type]
-    ) : (
-        <Text />
-    )
+  const board =
+    code >= 0
+      ? {
+          0: '잠시만 기다려주세요!',
+          1: '회원님의 계정이 선수 계정으로 전환되었어요!',
+          2: '반려 된 이유를 확인하시고 재등록 해주세요.',
+        }[code]
+      : ''
 
-    const board =
-    type >= 0 ? (
-        {
-            0: '잠시만 기다려주세요!',
-            1: '회원님의 계정이 선수 계정으로 전환되었어요!',
-            2: '반려 된 이유를 확인하시고 재등록 해주세요.',
-        }[type]
-    ) : (
-        <Text />
-    )
-    var content =
-    type >= 0 ? (
-        {
-            0: '심사는 3~5일이 소요됩니다.\n팬들과의 만남이 얼마 남지 않았어요{`><!`}',
-            1: '선수 계정만이 가진 특별한 기능을 통해 팬들과 더욱 자유롭게 소통하실 수 있어요! \n\n앞으로 라인업+와 함께 그려나갈 멋진 선수님의 모습 기대할께요^^',
-        }[type]
-    ) : (
-        <Text />
-        
-    )
-    if (type == 2)
-        content = `반려 사유: ${data.refuseContent}`
+  const content =
+    code >= 0
+      ? {
+          0: '심사는 3~5일이 소요됩니다.\n팬들과의 만남이 얼마 남지 않았어요{`><!`}',
+          1: '선수 계정만이 가진 특별한 기능을 통해 팬들과 더욱 자유롭게 소통하실 수 있어요! \n\n앞으로 라인업+와 함께 그려나갈 멋진 선수님의 모습 기대할께요^^',
+        }[code]
+      : ''
 
-    const onPress = () => {
-        navigation.navigate('PlayerRegistForm')
-    }
-    return (
-        <View style={{flexDirection: 'column'}}>
-            <Image source={DefaultProfileImage} />
-            <Text>{head}</Text>
-            <Text>{board}</Text>
-            <Text>{content}</Text>
-            {type == 2 && (
-                <TouchableOpacity onPress={() => onPress()}>
-                    <Text>재등록</Text>
-                </TouchableOpacity>
-            )}
+  if (code == 2) content = `반려 사유: ${data.refuseContent}`
+
+  const onPress = () => {
+    navigation.navigate('PlayerRegist')
+  }
+  return (
+    <View style={styles.playerRegistResultWrapper}>
+      <View style={styles.playerRegistInner}>
+        <View style={styles.playerRegistTop}>
+          <Image source={image} />
+          <Text style={styles.title}>{head}</Text>
+          <Text style={styles.subtitle}>{board}</Text>
+          <Text style={styles.content}>{content}</Text>
         </View>
-    )
+        {code == 2 && (
+          <TouchableOpacity onPress={() => onPress()} style={styles.button}>
+            <Text style={styles.buttonText}>재등록</Text>
+          </TouchableOpacity>
+        )}
+      </View>
+    </View>
+  )
 }
