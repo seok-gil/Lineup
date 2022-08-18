0794a4dff723f59ae8c4013279d2914007e2d8ed
chichoon <chichoon.choi@gmail.com>
2022-08-18 15:06:13 +0900

    Design: FeedBody

--- SCON/Views/Feed/FeedBody.js
+++ SCON/Views/Feed/FeedBody.js
@@ -1,65 +1,84 @@
 import React, {useEffect, useState} from 'react'
-import {View, TouchableOpacity, DeviceEventEmitter, Text, Image} from 'react-native'
-import {CommentIcon, HeartEmptyIcon} from '../../Assets/Icons'
+import {
+  View,
+  TouchableOpacity,
+  DeviceEventEmitter,
+  Text,
+  Image,
+} from 'react-native'
+import {CommentIcon, HeartEmptyIcon, HeartIcon} from '../../Assets/Icons'
 import {TimeRelative} from '../../Components/Time'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import {ApiFetch, LikeComponent} from '../../Components/'
 
+import styles from './FeedBody.styles'
+
 export function FeedBody({data, feedId, navigation, setMount}) {
-    var likeUrl = !data.ilike ? `feed/${data.feedId}` : `feed/${data.ilike}`
+  var likeUrl = !data.ilike ? `feed/${data.feedId}` : `feed/${data.ilike}`
 
-    const delFeed = () => {
-        AsyncStorage.getItem('accessToken').then(thing => {
-            ApiFetch({
-                method: 'DELETE',
-                url: `/player/feed/${feedId}`,
-                headers: {
-                    'content-type': 'application/json',
-                    Authorization: 'Bearer ' + thing,
-                },
-                body: null,
-            }).then(() => {
-                navigation.goBack()
-            })
-        })
-    }
+  const delFeed = () => {
+    AsyncStorage.getItem('accessToken').then(thing => {
+      ApiFetch({
+        method: 'DELETE',
+        url: `/player/feed/${feedId}`,
+        headers: {
+          'content-type': 'application/json',
+          Authorization: 'Bearer ' + thing,
+        },
+        body: null,
+      }).then(() => {
+        navigation.goBack()
+      })
+    })
+  }
 
-    const editFeed = () => {
-        navigation.navigate('FeedRegist', {
-            content: data.content,
-            feedId: data.feedId,
-            type: 'PUT',
-        })
-    }
-    return (
-        <View>
-            <Text>{data.content}</Text>
-            <Text>
-                <TimeRelative time={data.createDate} />
-            </Text>
-            <View style={{flexDirection: 'row'}}>
-                <TouchableOpacity
-                setMount={setMount}
-                    onPress={() => LikeComponent(data.ilike, likeUrl, setMount)}
-                    style={{flexDirection: 'row'}}>
-                    <Image source={data.ilike ? CommentIcon : HeartEmptyIcon} />
-                    <Text>{data.likeCnt}</Text>
-                </TouchableOpacity>
-                <View style={{flexDirection: 'row'}}>
-                    <Image source={CommentIcon} />
-                    <Text>{data.commentCnt}</Text>
-                </View>
-                <TouchableOpacity
-                    onPress={() => editFeed()}
-                    style={{flexDirection: 'row'}}>
-                    <Text>수정</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    onPress={() => delFeed()}
-                    style={{flexDirection: 'row'}}>
-                    <Text>삭제</Text>
-                </TouchableOpacity>
-            </View>
+  const editFeed = () => {
+    navigation.navigate('FeedRegist', {
+      content: data.content,
+      feedId: data.feedId,
+      type: 'PUT',
+    })
+  }
+  return (
+    <View style={styles.feedBodyWrapper}>
+      <View style={styles.contentWrapper}>
+        <Text style={styles.content}>{data.content}</Text>
+        <View style={styles.time}>
+          <Text style={styles.timeText}>
+            <TimeRelative time={data.createDate} />
+          </Text>
         </View>
-    )
+      </View>
+      <View style={styles.commentButtons}>
+        <View style={styles.icons}>
+          <TouchableOpacity
+            setMount={setMount}
+            onPress={() => LikeComponent(data.ilike, likeUrl, setMount)}
+            style={styles.iconWrapper}>
+            <Image
+              style={styles.icon}
+              source={data.ilike ? HeartIcon : HeartEmptyIcon}
+            />
+            <Text style={styles.text}>{data.likeCnt}</Text>
+          </TouchableOpacity>
+          <View style={styles.iconWrapper}>
+            <Image style={styles.icon} source={CommentIcon} />
+            <Text style={styles.text}>{data.commentCnt}</Text>
+          </View>
+        </View>
+        <View style={styles.modify}>
+          <TouchableOpacity
+            onPress={() => editFeed()}
+            style={styles.modifyButton}>
+            <Text style={styles.text}>수정</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => delFeed()}
+            style={styles.modifyButton}>
+            <Text style={styles.text}>삭제</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  )
 }
--- SCON/Views/Feed/index.js
+++ SCON/Views/Feed/index.js
@@ -2,43 +2,42 @@ import React, {useEffect, useState} from 'react'
 import {ApiFetch} from '../../Components/API/ApiFetch'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import {View, TouchableOpacity, SafeAreaView, Text, Image} from 'react-native'
-import { FeedApi } from './FeedApi'
+import {FeedApi} from './FeedApi'
 import {CommentList} from './Comment'
 import {FeedBody} from './FeedBody'
 
-export function FeedScreen({route, navigation}) {
-    const [data, setData] = useState()
-    const [mount, setMount] = useState(new Date())
-    
-    useEffect(() => {
-        AsyncStorage.getItem('accessToken').then(thing => {
-            ApiFetch({
-                method: 'GET',
-                url: `/feed/${route.params.feedId}`,
-                headers: {
-                    'content-type': 'application/json',
-                    Authorization: 'Bearer ' + thing,
-                },
-                body: null,
-            }).then(thing => {
-                setData(thing)
-            })
-        })
-    }, [mount])
+import styles from './Feed.styles'
 
-    if (!data) return <SafeAreaView />
-    return (
-        <SafeAreaView style={{flex: 3, flexDirection: 'column'}}>
-            <FeedBody
-                data={data}
-                feedId={route.params.feedId}
-                navigation={navigation}
-                setMount={setMount}
-            />
-            <CommentList
-                feedId={route.params.feedId}
-                navigation={navigation}
-            />
-        </SafeAreaView>
-    )
+export function FeedScreen({route, navigation}) {
+  const [data, setData] = useState()
+  const [mount, setMount] = useState(new Date())
+
+  useEffect(() => {
+    AsyncStorage.getItem('accessToken').then(thing => {
+      ApiFetch({
+        method: 'GET',
+        url: `/feed/${route.params.feedId}`,
+        headers: {
+          'content-type': 'application/json',
+          Authorization: 'Bearer ' + thing,
+        },
+        body: null,
+      }).then(thing => {
+        setData(thing)
+      })
+    })
+  }, [mount])
+
+  if (!data) return <SafeAreaView />
+  return (
+    <SafeAreaView style={styles.feedWrapper}>
+      <FeedBody
+        data={data}
+        feedId={route.params.feedId}
+        navigation={navigation}
+        setMount={setMount}
+      />
+      <CommentList feedId={route.params.feedId} navigation={navigation} />
+    </SafeAreaView>
+  )
 }
