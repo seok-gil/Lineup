7d62345a79dfd990560e09b2fd32219042cf3920
chichoon <chichoon.choi@gmail.com>
2022-08-22 01:31:48 +0900

    Design: svgs

--- SCON/Views/Search/SearchInput.js
+++ SCON/Views/Search/SearchInput.js
@@ -2,30 +2,30 @@ import React, {useRef} from 'react'
 import {Button, View, TextInput, Image} from 'react-native'
 
 import styles from './SearchInput.styles'
-import {SearchIcon} from '../../Assets/Icons'
+import {SearchIcon} from '../../Assets/svgs'
 
 function SearchInput({inputs, setInputs}) {
-    const searchRef = useRef()
+  const searchRef = useRef()
 
-    const onChange = (keyvalue, e) => {
-        const {text} = e.nativeEvent
-        setInputs({
-            ...inputs,
-            [keyvalue]: text,
-        })
-    }
-    return (
-        <View style={styles.searchInputWrapper}>
-            <Image source={SearchIcon} style={styles.searchInputIcon} />
-            <TextInput
-                value={inputs.search}
-                placeholder={'검색'}
-                placeholderTextColor="#0E0E0E66"
-                onChange={e => onChange('search', e)}
-                onSubmitEditing={() => searchRef.current.focus()}
-            />
-        </View>
-    )
+  const onChange = (keyvalue, e) => {
+    const {text} = e.nativeEvent
+    setInputs({
+      ...inputs,
+      [keyvalue]: text,
+    })
+  }
+  return (
+    <View style={styles.searchInputWrapper}>
+      <SearchIcon style={styles.searchInputIcon} />
+      <TextInput
+        value={inputs.search}
+        placeholder={'검색'}
+        placeholderTextColor="#0E0E0E66"
+        onChange={e => onChange('search', e)}
+        onSubmitEditing={() => searchRef.current.focus()}
+      />
+    </View>
+  )
 }
 
 export default SearchInput
