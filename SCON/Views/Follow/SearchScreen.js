5aa6a1a12239b7db119485c105efca59a0771b1c
chichoon <chichoon.choi@gmail.com>
2022-08-22 01:33:42 +0900

    Design: searchIcon svg

--- SCON/Views/Follow/SearchScreen.js
+++ SCON/Views/Follow/SearchScreen.js
@@ -1,65 +1,57 @@
 import React, {Component, useState, useRef} from 'react'
 
-import {
-    View,
-    TouchableOpacity,
-    StyleSheet,
-    Text,
-    Image,
-    TextInput,
-    SafeAreaView,
-} from 'react-native'
+import {View, TouchableOpacity, Text, Image, TextInput} from 'react-native'
 
-import {SearchIcon} from '../../Assets/Icons'
+import {SearchIcon} from '../../Assets/svgs'
 import styles from './SearchScreen.styles'
 
 export function SearchId({data, navigation}) {
-    if (!data) {
-        return <View />
-    }
-    return (
-        <TouchableOpacity
-            onPress={() =>
-                navigation.navigate('FollowPage', {userId: data.memberId})
-            }>
-            <View style={styles.viewPlayerWrapper}>
-                <Image source={{uri: data.profilePic}} style={styles.viewPlayerImage} />
-                <View style={styles.viewPlayerInfo}>
-                    <Text
-                        style={styles.playerName}
-                        numberOfLines={1}
-                        ellipsizeMode="tail">
-                        {data.nickname}
-                    </Text>
-                </View>
-            </View>
-        </TouchableOpacity>
-    )
+  if (!data) {
+    return <View />
+  }
+  return (
+    <TouchableOpacity
+      onPress={() =>
+        navigation.navigate('FollowPage', {userId: data.memberId})
+      }>
+      <View style={styles.viewPlayerWrapper}>
+        <Image source={{uri: data.profilePic}} style={styles.viewPlayerImage} />
+        <View style={styles.viewPlayerInfo}>
+          <Text
+            style={styles.playerName}
+            numberOfLines={1}
+            ellipsizeMode="tail">
+            {data.nickname}
+          </Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  )
 }
 
 export function SearchInput() {
-    const searchRef = useRef()
-    const [inputs, setInputs] = useState({
-        search: '',
+  const searchRef = useRef()
+  const [inputs, setInputs] = useState({
+    search: '',
+  })
+  const onChange = (keyvalue, e) => {
+    const {text} = e.nativeEvent
+    setInputs({
+      ...inputs,
+      [keyvalue]: text,
     })
-    const onChange = (keyvalue, e) => {
-        const {text} = e.nativeEvent
-        setInputs({
-            ...inputs,
-            [keyvalue]: text,
-        })
-    }
+  }
 
-    return (
-        <View style={styles.searchInputWrapper}>
-            <Image source={SearchIcon} style={styles.searchInputIcon} />
-            <TextInput
-                value={inputs.search}
-                placeholder={'검색'}
-                placeholderTextColor="#0E0E0E66"
-                onChange={e => onChange('search', e)}
-                onSubmitEditing={() => searchRef.current.focus()}
-            />
-        </View>
-    )
+  return (
+    <View style={styles.searchInputWrapper}>
+      <SearchIcon style={styles.searchInputIcon} />
+      <TextInput
+        value={inputs.search}
+        placeholder={'검색'}
+        placeholderTextColor="#0E0E0E66"
+        onChange={e => onChange('search', e)}
+        onSubmitEditing={() => searchRef.current.focus()}
+      />
+    </View>
+  )
 }
