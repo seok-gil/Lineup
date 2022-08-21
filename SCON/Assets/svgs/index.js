eddb0ccd89b7e3be8293b5c86aa78cf13c59ce20
chichoon <chichoon.choi@gmail.com>
2022-08-21 10:34:57 +0900

    Design: WelcomeImage svg로 변경

--- SCON/Assets/svgs/index.js
+++ SCON/Assets/svgs/index.js
@@ -1,2 +1,4 @@
 export {default as LineupLogo} from './lineupLogo.svg'
 export {default as MedalIcon} from './medal.svg'
+export {default as checkIcon} from './checkIcon.svg'
+export {default as WelcomeImage} from './welcome.svg'
--- SCON/Views/Login/Regist/RegistModal.js
+++ SCON/Views/Login/Regist/RegistModal.js
@@ -1,36 +1,36 @@
 import React, {useState} from 'react'
 import {
-    SafeAreaView,
-    TouchableOpacity,
-    View,
-    Text,
-    Image,
-    Modal,
+  SafeAreaView,
+  TouchableOpacity,
+  View,
+  Text,
+  Image,
+  Modal,
 } from 'react-native'
 import styles from './RegistModal.styles'
 
-import {WelcomeIcon} from '../../../Assets/Icons'
+import {WelcomeImage} from '../../../Assets/svgs'
 
 export function RegistModal({modal, setModal, navigation}) {
-    const onPress = () => {
-        navigation.navigate('LoginPage')
-        setModal(false)
-    }
-    return (
-        <Modal visible={modal}>
-            <View style={styles.centeredView}>
-                <View style={styles.modalView}>
-                    <View style={styles.modalTop}>
-                        <Image source={WelcomeIcon} style={styles.modalImage} />
-                        <Text style={styles.modalText}>라인업에 오신 것을 환영해요!</Text>
-                    </View>
-                    <View>
-                        <TouchableOpacity style={styles.modalBottom} onPress={onPress}>
-                            <Text style={styles.modalText}>확인</Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-            </View>
-        </Modal>
-    )
+  const onPress = () => {
+    navigation.navigate('LoginPage')
+    setModal(false)
+  }
+  return (
+    <Modal visible={modal}>
+      <View style={styles.centeredView}>
+        <View style={styles.modalView}>
+          <View style={styles.modalTop}>
+            <WelcomeImage width={120} height={30} style={styles.modalImage} />
+            <Text style={styles.modalText}>라인업에 오신 것을 환영해요!</Text>
+          </View>
+          <View>
+            <TouchableOpacity style={styles.modalBottom} onPress={onPress}>
+              <Text style={styles.modalText}>확인</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </Modal>
+  )
 }
