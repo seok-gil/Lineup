7fc188490f38d9c4694db683b69733674b52509a
chichoon <chichoon.choi@gmail.com>
2022-08-21 13:42:42 +0900

    Feat: Mypage Tab svg로 수정

--- SCON/Views/MyPage/MypageTab.js
+++ SCON/Views/MyPage/MypageTab.js
@@ -1,55 +1,75 @@
 import React from 'react'
-import {View, Text, Image, TouchableOpacity} from 'react-native'
+import {View, Text, TouchableOpacity} from 'react-native'
 
 import {
-    QnAIcon,
-    NotiIcon,
-    MegaphoneIcon,
-    LogoutIcon,
-    DownIcon,
-} from '../../Assets/Icons'
+  LogoutIcon,
+  MegaphoneIcon,
+  QnAIcon,
+  ArrowIcon,
+  NotiIcon,
+} from '../../Assets/svgs'
 
 import styles from './MypageTab.styles'
 
 export function MypageTab({navigation}) {
-    return (
-        <View style={styles.mypageTabWrapper}>
-            <TouchableOpacity
-                style={styles.mypageElement}
-                onPress={() => navigation.navigate('Noti')}>
-                <View style={styles.elementLeft}>
-                    <Image source={MegaphoneIcon} style={styles.elementIcon} />
-                    <Text style={styles.elementText}>공지사항</Text>
-                </View>
-                <Image source={DownIcon} style={styles.arrowIcon} />
-            </TouchableOpacity>
-            <TouchableOpacity
-                style={styles.mypageElement}
-                onPress={() => navigation.navigate('SettingAlert')}>
-                <View style={styles.elementLeft}>
-                    <Image source={NotiIcon} style={styles.elementIcon} />
-                    <Text style={styles.elementText}>알림 설정</Text>
-                </View>
-                <Image source={DownIcon} style={styles.arrowIcon} />
-            </TouchableOpacity>
-            <TouchableOpacity
-                style={styles.mypageElement}
-                onPress={() => navigation.navigate('InquiryTab')}>
-                <View style={styles.elementLeft}>
-                    <Image source={QnAIcon} style={styles.elementIcon} />
-                    <Text style={styles.elementText}>문의 하기</Text>
-                </View>
-                <Image source={DownIcon} style={styles.arrowIcon} />
-            </TouchableOpacity>
-            <TouchableOpacity
-                style={styles.mypageElement}
-                onPress={() => navigation.navigate('AccountScreen')}>
-                <View style={styles.elementLeft}>
-                    <Image source={LogoutIcon} style={styles.elementIcon} />
-                    <Text style={styles.elementText}>계정 관리</Text>
-                </View>
-                <Image source={DownIcon} style={styles.arrowIcon} />
-            </TouchableOpacity>
+  return (
+    <View style={styles.mypageTabWrapper}>
+      <TouchableOpacity
+        style={styles.mypageElement}
+        onPress={() => navigation.navigate('Noti')}>
+        <View style={styles.elementLeft}>
+          <MegaphoneIcon width={18} height={18} style={styles.elementIcon} />
+          <Text style={styles.elementText}>공지사항</Text>
         </View>
-    )
+        <ArrowIcon
+          width={11}
+          height={6}
+          fill={'#0E0E0E'}
+          style={styles.arrowIcon}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.mypageElement}
+        onPress={() => navigation.navigate('SettingAlert')}>
+        <View style={styles.elementLeft}>
+          <NotiIcon width={18} height={18} style={styles.elementIcon} />
+          <Text style={styles.elementText}>알림 설정</Text>
+        </View>
+        <ArrowIcon
+          width={11}
+          height={6}
+          fill={'#0E0E0E'}
+          style={styles.arrowIcon}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.mypageElement}
+        onPress={() => navigation.navigate('InquiryTab')}>
+        <View style={styles.elementLeft}>
+          <QnAIcon width={18} height={18} style={styles.elementIcon} />
+          <Text style={styles.elementText}>문의 하기</Text>
+        </View>
+        <ArrowIcon
+          width={11}
+          height={6}
+          fill={'#0E0E0E'}
+          style={styles.arrowIcon}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.mypageElement}
+        onPress={() => navigation.navigate('AccountScreen')}>
+        <View style={styles.elementLeft}>
+          <LogoutIcon width={18} height={18} style={styles.elementIcon} />
+          <Text style={styles.elementText}>계정 관리</Text>
+        </View>
+        <ArrowIcon
+          width={11}
+          height={6}
+          fill={'#0E0E0E'}
+          style={styles.arrowIcon}
+        />
+      </TouchableOpacity>
+    </View>
+  )
 }
