93cdad3b3267fdf3ef539eb1424a7d0b1dddcd72
chichoon <chichoon.choi@gmail.com>
2022-08-25 16:29:15 +0900

    Feat: PlayerRegistList

---
+++ SCON/Views/Admin/PlayerRegist/PlayerRegistList/PlayerRegistList.styles.js
@@ -0,0 +1,11 @@
+import {StyleSheet} from 'react-native'
+import {colors} from '../../../../Styles'
+
+const styles = StyleSheet.create({
+  playerRegistListWrapper: {
+    backgroundColor: colors.WHITE,
+    height: '100%',
+  },
+})
+
+export default styles
