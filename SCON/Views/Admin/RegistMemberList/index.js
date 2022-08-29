da460c1772179578e133bd02b56709cde5ded55e
chichoon <chichoon.choi@gmail.com>
2022-08-30 00:37:46 +0900

    Design: 상단 탭 바 색상 변경

--- SCON/Views/Admin/RegistMemberList/index.js
+++ SCON/Views/Admin/RegistMemberList/index.js
@@ -8,10 +8,11 @@ import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs
 const RegistMemberStack = createMaterialTopTabNavigator()
 
 export function RegistMemberTabScreen() {
-    return (
-        <RegistMemberStack.Navigator>
-            <RegistMemberStack.Screen name="일반 계정" component={UserList} />
-            <RegistMemberStack.Screen name="선수 계정" component={PlayerList}/>
-        </RegistMemberStack.Navigator>
-    )
+  return (
+    <RegistMemberStack.Navigator
+      screenOptions={{tabBarIndicatorStyle: {backgroundColor: '#17D3F0'}}}>
+      <RegistMemberStack.Screen name="일반 계정" component={UserList} />
+      <RegistMemberStack.Screen name="선수 계정" component={PlayerList} />
+    </RegistMemberStack.Navigator>
+  )
 }
