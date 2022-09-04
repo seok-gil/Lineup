90aeca4e1bcc2939d944daf2d7530397595f77a3
chichoon <chichoon.choi@gmail.com>
2022-09-05 00:22:27 +0900

    Design: 대회결과 스타일 수정

--- Lineup/Views/Story/CompetitionResult/CompetitionElement.js
+++ Lineup/Views/Story/CompetitionResult/CompetitionElement.js
@@ -5,31 +5,30 @@ import styles from './CompetitionElement.styles'
 import {Time} from '../../../Components/Time'
 
 const CompetitionElement = ({data, navigation}) => {
-    if (!data) return <View />
-    return (
-        <View style={styles.competitionElementWrapper}>
-            <View style={styles.competitionElementTexts}>
-                <Text style={styles.dataName}> {data.eventName}</Text>
-                <Text style={styles.dataDate}>
-                    {' '}
-                    <Time time={data.startDate} separator="-" />{' '}
-                </Text>
-            </View>
-            <TouchableOpacity
-                onPress={() =>
-                    navigation.navigate('CompetitionResultDetail', {
-                        data: data,
-                        type: data.existResult,
-                    })
-                }>
-                {!data.existResult ? (
-                    <Text style={styles.modifyText}> 결과 입력</Text>
-                ) : (
-                    <Text style={styles.modifyText}> 수정 하기</Text>
-                )}
-            </TouchableOpacity>
-        </View>
-    )
+  if (!data) return <View />
+  return (
+    <View style={styles.competitionElementWrapper}>
+      <View style={styles.competitionElementTexts}>
+        <Text style={styles.dataName}> {data.eventName}</Text>
+        <Text style={styles.dataDate}>
+          <Time time={data.startDate} separator="-" />
+        </Text>
+      </View>
+      <TouchableOpacity
+        onPress={() =>
+          navigation.navigate('CompetitionResultDetail', {
+            data: data,
+            type: data.existResult,
+          })
+        }>
+        {!data.existResult ? (
+          <Text style={styles.modifyText}>결과 입력</Text>
+        ) : (
+          <Text style={styles.modifyText}>수정하기</Text>
+        )}
+      </TouchableOpacity>
+    </View>
+  )
 }
 
 export default CompetitionElement
