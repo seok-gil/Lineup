30f237e998b8139b3f4a7cf29a937e77ad1d5794
chichoon <chichoon.choi@gmail.com>
2022-09-05 20:06:05 +0900

    Design: 약관 모달

--- Lineup/Views/Login/Regist/Accept/AcceptModal.js
+++ Lineup/Views/Login/Regist/Accept/AcceptModal.js
@@ -1,28 +1,54 @@
 import React from 'react'
-import { TouchableOpacity, SafeAreaView, ScrollView, View, Text, Modal, Touchable } from 'react-native'
-import { Privacy } from './Assets/Privacy'
-import { Service } from './Assets/Service'
-export function AcceptModal({ modal, setModal, accept }) {
-  
+import {
+  TouchableOpacity,
+  SafeAreaView,
+  ScrollView,
+  View,
+  Text,
+  Modal,
+  Touchable,
+} from 'react-native'
+import {Privacy} from './Assets/Privacy'
+import {Service} from './Assets/Service'
+
+import styles from './AcceptModal.styles'
+
+export function AcceptModal({modal, setModal, accept}) {
   const onPress = () => {
     setModal(false)
   }
   return (
     <Modal visible={modal}>
-      <SafeAreaView>
-        <ScrollView>
-          <Text style={{ flexShrink: 1, flexWrap: 'wrap' }}>
-            {accept == 'service' ? <Service type={'title'}/> : <Privacy type={'title'}/>}
-          </Text>
-          <Text style={{ flexShrink: 1, flexWrap: 'wrap' }}>
-            {accept == 'service' ? <Service type={'body'}/> : <Privacy type={'body'}/>}
-          </Text>
-            <TouchableOpacity onPress={() => onPress()}>
-              <Text>X</Text>
-            </TouchableOpacity>
-        </ScrollView>
+      <SafeAreaView style={styles.centeredView}>
+        <View style={styles.modalWrapper}>
+          <ScrollView style={styles.modalInnerScroll}>
+            {accept == 'service' ? (
+              <>
+                <Text style={styles.title}>
+                  <Service type={'title'} />
+                </Text>
+                <Text style={styles.body}>
+                  <Service type={'body'} />
+                </Text>
+              </>
+            ) : (
+              <>
+                <Text style={styles.title}>
+                  <Privacy type={'title'} />
+                </Text>
+                <Text style={styles.body}>
+                  <Privacy type={'body'} />
+                </Text>
+              </>
+            )}
+          </ScrollView>
+          <TouchableOpacity
+            onPress={() => onPress()}
+            style={styles.buttonStyle}>
+            <Text style={styles.buttonTextStyle}>확인</Text>
+          </TouchableOpacity>
+        </View>
       </SafeAreaView>
     </Modal>
   )
 }
-
