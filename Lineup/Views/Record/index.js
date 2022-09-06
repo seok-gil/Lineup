5d929faf3c39fc1e92f67638d55f48c23b13216c
hopark123 <polkm789@naver.com>
2022-09-06 20:44:53 +0900

    feat : record data paging

--- Lineup/Views/Record/index.js
+++ Lineup/Views/Record/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { SafeAreaView, ScrollView, View } from 'react-native'
+import { SafeAreaView, ScrollView, FlatList, View } from 'react-native'
 import { ApiFetch } from '../../Components/API/ApiFetch'
 
 import RecordHead from './RecordHead'
@@ -10,12 +10,14 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 
 export function RecordScreen({ route }) {
   const [data, setData] = useState([])
+  const [lastId, setLastId] = useState(100000)
+  const [size, setSize] = useState(10)
 
   useEffect(() => {
     AsyncStorage.getItem('accessToken').then(thing => {
       ApiFetch({
         method: 'GET',
-        url: `/player-home/${route.params.playerId}/records`,
+        url: `/player-home/${route.params.playerId}/records?size=${size}&lastId=${lastId}`,
         headers: {
           'content-type': 'application/json',
           Authorization: 'Bearer ' + thing,
@@ -25,24 +27,34 @@ export function RecordScreen({ route }) {
         if (thing == 401) {
           navigation.navigate('RefreshTokenModal', { navigation: navigation })
         }
-        else setData(thing)
+        else setData(thing.content)
       })
     })
-  }, [])
+  }, [size])
+  const onEndReached = (e) => {
+    setSize(size + 3)
+  }
 
   if (!data) return <SafeAreaView />
   return (
     <SafeAreaView style={styles.recordScreenWrapper}>
       <ScrollView>
         <RecordHead data={data} />
-        <View>
-          {data.map(record => (
+        <FlatList
+          data={data}
+          snapToAlignment="start"
+          decelerationRate="fast"
+          renderItem={({ item, index }) => (
             <PlayerCard
-              key={`player-card-${record.recordId}`}
-              record={record}
+              key={`record-card-${index}`}
+              record={item}
             />
-          ))}
-        </View>
+          )}
+          onEndReached={(e) => onEndReached(e)}
+          onEndReachedThreshold={0.9}
+          onMomentumScrollBegin={() => { this.onEndReachedCalledDuringMomentum = false; }}
+          showsHorizontalScrollIndicator={false}
+        />
       </ScrollView>
     </SafeAreaView>
   )
